8ccb1ace1210830c5128d0908d1a141d3699d338
Juan <jcruiz1245@gmail.com>
2023-04-22 20:36:37 -0600

    Configurando admin

--- miSitio/mi_app/models.py
+++ miSitio/mi_app/models.py
@@ -5,7 +5,13 @@ from django.db import models
 class usuario(models.Model):
 	user = models.CharField(max_length=50)
 
+	def __str__(self):
+		return self.user
+
 class tarea(models.Model):
 	titulo = models.CharField(max_length=100)
 	descripcion = models.TextField()
 	proyecto = models.ForeignKey(usuario,on_delete=models.CASCADE)
+
+	def __str__(self):
+		return self.titulo + " - " + self.descripcion
